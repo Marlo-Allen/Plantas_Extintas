f4e26471cddc349a0cd98de88826d5f8e80242a1
Marlo-Allen <83998309+Marlo-Allen@users.noreply.github.com>
2021-05-12 21:51:31 -0400

    Agregue el rmd para explorar plantas extintas

--- Clase_2.R
+++ Clase_2.R
@@ -1,3 +1,8 @@
 library(tidyverse)
 library(kableExtra)
 library(knitr)
+
+
+plants<- read.csv("C:/Users/mhrosas/Documents/GitHub/tidytuesday/data/2020/2020-08-18/plants.csv", sep=";")
+actions<- read.csv("C:/Users/mhrosas/Documents/GitHub/tidytuesday/data/2020/2020-08-18/actions.csv", sep=";")
+threats<- read.csv("C:/Users/mhrosas/Documents/GitHub/tidytuesday/data/2020/2020-08-18/threats.csv", sep=";")
